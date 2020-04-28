1c1ec1f940c6d9789ad8a4b26e0ceeef60403b1d
Amanda Gomes <amanda89conceicao@gmail.com>
2020-04-28 20:51:48 -0300

    teste do fabricio

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Olá Amanda!"
+    return "O Fabricio é Lindo!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
